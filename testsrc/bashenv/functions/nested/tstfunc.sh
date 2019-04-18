efaca824cade8b014faef43b49687d7ecc9b2331
Eric Welton <eric@korsimoro.com>
2019-04-18 20:59:29 +0700

    update test template

--- testsrc/bashenv/functions/nested/tstfunc.sh
+++ testsrc/bashenv/functions/nested/tstfunc.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-function oneline_help_kx_nested_tstfunc() {
+function oneline_help_SHELL_PREFIX_nested_tstfunc() {
   echo "nested_tstfunc home, or into $VAR_PREFIX/[C]."
 }
 
-function help_kx_nested_tstfunc() {
+function help_SHELL_PREFIX_nested_tstfunc() {
 printf "`cat << EOF
-${BLUE}kx nested_tstfunc <repo>${NC}
+${BLUE}SHELL_PREFIX nested_tstfunc <repo>${NC}
 
 This is a shell function, which changes the current working
 directory to $PREFIX/<repo>
@@ -16,7 +16,7 @@ EOF
 `\n\n"
 }
 
-function run_kx_nested_tstfunc() {
+function run_SHELL_PREFIX_nested_tstfunc() {
   echo "running nested_tstfunc"
 }
-export -f run_kx_nested_tstfunc help_kx_nested_tstfunc oneline_help_kx_nested_tstfunc
+export -f run_SHELL_PREFIX_nested_tstfunc help_SHELL_PREFIX_nested_tstfunc oneline_help_SHELL_PREFIX_nested_tstfunc
--- testsrc/bashenv/functions/tstfunc.sh
+++ testsrc/bashenv/functions/tstfunc.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-function oneline_help_kx_tstfunc() {
+function oneline_help_SHELL_PREFIX_tstfunc() {
   echo "tstfunc home, or into $VAR_PREFIX/[C]."
 }
 
-function help_kx_tstfunc() {
+function help_SHELL_PREFIX_tstfunc() {
 printf "`cat << EOF
 ${BLUE}kx tstfunc <repo>${NC}
 
@@ -16,7 +16,7 @@ EOF
 `\n\n"
 }
 
-function run_kx_tstfunc() {
+function run_SHELL_PREFIX_tstfunc() {
   echo "running tstfunc"
 }
-export -f run_kx_tstfunc help_kx_tstfunc oneline_help_kx_tstfunc
+export -f run_SHELL_PREFIX_tstfunc help_SHELL_PREFIX_tstfunc oneline_help_SHELL_PREFIX_tstfunc
